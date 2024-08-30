ee4ac3ebbc658a2abaaa2a4f7180f0c973ad8fc7
Noah Huls <hulsnoah@gmail.com>
2024-08-30 10:10:48 +0200

    updated api

--- PythonConnection.Python/PythonConnection.Python.py
+++ PythonConnection.Python/PythonConnection.Python.py
@@ -1,12 +1,20 @@
-from flask import Flask, jsonify
-import random
+from _pytest.config import console_main
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/hello-world', methods=['GET'])
-def hello_world_with_random():
-    random_number = random.randint(1, 100)
-    return jsonify(message="Hello, World!", random_number=random_number)
+class Prediction:
+    def __init__(self, prediction):
+        self.prediction = prediction
+
+@app.route('/api/predict/number', methods=['POST'])
+def predict_number():
+    data = request.get_json()
+    print(data)
+
+    prediction_result = Prediction(prediction=999)
+
+    return jsonify(message="Prediction:", data=prediction_result.__dict__)
 
 if __name__ == '__main__':
     app.run(debug=True)
